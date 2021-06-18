62f3b0f61dfa697ffaca5adf927f45708e968e7b
rihitosan <76610702+rihitosan@users.noreply.github.com>
2021-06-19 08:03:15 +0900

    Add files via upload

--- tw-hack.js
+++ tw-hack.js
@@ -2,34 +2,48 @@
 これはjavascriptから取得できるtwitter乗っ取りアプリ名リストです。
 不適切な言葉が含まれておりますが、ご了承ください。
 定期的に変更していきます。
-*********************************
-Dont't copy this source code.
 */
 
 var app_info = document.getElementsByClassName("app-info")[0].textContent //アプリ名が入ってる要素を取得
 
-//乗っ取りアプリ名リスト
 var n_list = ["あなたの価値早見表",
 "コミュニケーション能力テスト",
 "あなたの性的情報一覧",
 "あなたにあるもの、ないもの",
 "リアル結婚年齢",
+"あなたの理想の恋人像",
 "世界最強の顔採点大診断！",
-"顔面センター試験",
+"センター試験",
 "セック○診断",
 "あなたは一生何人と♡♡をする？",
 "エッチ度検査",
 "あなたの顔採点",
 "最新型 『顔面採点診断』",
 "あなたの性的嗜好一覧",
+"あなたのお値段",
+"顔面ランク",
+"あなたと相性が良い人の誕生日ベスト",
+"あなたのモテモテ度点数",
+"小学知識テスト",
 "Popularity App.",
 "twinewss",
 "バズ情報局！",
-"TWIPICKS"]
+"TWIPICKS",
+"トレンド速報+",
+"NEWSJP",
+"TWEETJAPAN",
+"twitte news+",
+"twitter news",
+"JustTweet!!",
+"ツイッタラー！",
+"ツイッターLOG",
+"TWILOG202",
+"人気ツイートタイム！",
+"JAPSNS",]
     
     
     for(var i = 0;i < n_list.length;i++){ //n_listの数回ループ
        if(app_info.indexOf(n_list[i]) != -1){ //乗っ取りアプリの名前が含まれてたら
-        document.getElementById("var_share").textContent = "1" //chrome拡張機能と変数を共有するためにhtmlの要素に入れている
+        document.getElementById("var_share").textContent = "1"
        }
     }
